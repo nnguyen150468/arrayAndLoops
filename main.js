db5dd73b40cab80ec8cac3cae67b13389c9aaea7
Nguyen Nguyen <n.dinhnguyen95@gmail.com>
2020-03-09 13:33:37 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const inventors = [
+    "Albert Einstein",
+    "Issac Newton",
+    "Galileo Galilei",
+    "Marie Curie",
+    "Johannes Kepler",
+    "Nicolaus Copernicus",
+    "Max Planck",
+    "Katherine Blodgett",
+    "Ada Lovelace",
+    "Sarah E. Goode",
+    "Lise Meitner",
+    "Hanna Hammarstrom"
+];
+  
+const numbers = [1, 2, 3, 5, 8, 13, 21, 34, 55];
